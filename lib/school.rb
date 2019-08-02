1296771904e0d01ac8966a3efb223b0c8349ab1e
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-02 23:46:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+
+class School
+  attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+  end
+  
+def name
+     @name 
+   end
+   
+def name=(string)
+     @name = string
+  end
+
+def add_student(string, num)
+  roster[num] ||= []
+  roster[num] << string
+  end
+
+def grade(num)
+  roster.detect do |x,y|
+    if x == num
+      return y
+    end
+  end
+end 
+
+def sort(students)
+  
+  end
+end
